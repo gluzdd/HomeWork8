afe720940301a69086966b705cf5388b221d7975
Artem <Artem@Nesterov.ru>
2023-06-19 18:47:13 +0300

    HomeWork8

---
+++ Program.cs
@@ -0,0 +1,232 @@
+﻿/*
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+
+Console.Write("Введите кол-во строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] resMatrix = GetMatrix(rows, columns, 0, 10);
+
+PrintMatrix(resMatrix);
+DescendingSort(resMatrix);
+PrintMatrix(resMatrix);
+
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+Console.WriteLine();
+}
+void DescendingSort(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int[,] matrix = new int[3, 4];
+
+FillArrayRandom(matrix);
+PrintArray(matrix);
+Console.WriteLine();
+TheSmallestSumOfElements(matrix);
+
+void FillArrayRandom(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void TheSmallestSumOfElements(int[,] matrix)
+{
+    int minRow = 0;
+    int minSumRow = 0;
+    int sumRow = 0;
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {
+        minRow = minRow + matrix[0, 1];
+    }
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sumRow = sumRow + matrix[i, j];
+        }
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSumRow = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSumRow + 1} - строка с наименьшей суммой элементов");
+}
+
+Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+Console.Write("Введите кол-во строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = GetMatrix(rows, columns, 0, 10);
+int[,] secondMatrix = GetMatrix(rows, columns, 0, 10);
+int[,] resMatrix = GetProductMatrix(matrix, secondMatrix);
+
+PrintMatrix(matrix);
+PrintMatrix(secondMatrix);
+PrintMatrix(resMatrix);
+
+int[,] GetMatrix(int m, int n, int min, int max)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+Console.WriteLine();
+}
+
+int[,] GetProductMatrix(int[,] matrix, int[,] secondMatrix)
+{
+    int[,] resMatrix = new int[matrix.GetLength(0), secondMatrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < secondMatrix.GetLength(1); j++) 
+        {
+            for (int k = 0; k < matrix.GetLength(1); k++) 
+            {
+                resMatrix[i, j] += matrix[i, k] * secondMatrix[k, j];
+            }
+        }
+    }
+    return resMatrix;
+}
+
+/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+
+int[,,] matrix3D = new int[3, 3, 3];
+
+FillArray(matrix3D);
+PrintIndex(matrix3D);
+
+void PrintIndex(int[,,] arr)
+{
+    for (int i = 0; i < matrix3D.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix3D.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < matrix3D.GetLength(2); k++)
+            {
+                Console.Write($"{matrix3D[i, j, k]}({i},{j},{k}) ");
+            }
+        }
+    }
+}
+void FillArray(int[,,] matr)
+{
+    int count = 10;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            for (int k = 0; k < matr.GetLength(2); k++)
+            {
+                matr[k, i, j] += count;
+                count += 3;
+            }
+        }
+    }
+}
+*/
